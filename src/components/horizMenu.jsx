bfc229d98af2206555a7ecf7f8c284e5cc03d986
Alessio Carnevale <alessio.carnevale@gmail.com>
2018-12-07 15:53:13 +0100

    fixing horizMenu

--- src/components/horizMenu.jsx
+++ src/components/horizMenu.jsx
@@ -6,12 +6,20 @@ const _HorizMenu = styled.div`
   &:after {
     content: '';
     position: absolute;
-    top: 0px;
+    top: 10px;
     right: -20px;
     width: 20px;
     background-color: #000;
-    height: 100%;
+    bottom: 10px;
     box-shadow: -10px 0px 30px #000;
+    border-left: 1px dotted var(--gold);
+    display: none;
+  }
+
+  &.overflows {
+    &:after {
+      display: block;
+    } 
   }
 
   .scrollable {
@@ -31,18 +39,43 @@ const _HorizMenu = styled.div`
 _HorizMenu.displayName = 'HorizMenu'
 
 export default class HorizMenu extends PureComponent {
+  constructor (props) {
+    super(props)
+    this.scrollableRef = React.createRef()
+
+    this.state = {
+      overflows: false
+    }
+
+    if (typeof window !== 'undefined') {
+      window.addEventListener('resize', (e) => {
+        this.setState({
+          overflows: this.scrollableRef.current.offsetWidth !== this.scrollableRef.current.scrollWidth
+        })
+      })
+    }
+  }
+
   scrollTo (e) {
     e.preventDefault()
 
     document.querySelector(`${e.target.hash}`).scrollIntoView({ behavior: 'smooth', block: 'start' })
   }
+
+  componentDidMount () {
+    this.setState({
+      overflows: this.scrollableRef.current.offsetWidth !== this.scrollableRef.current.scrollWidth
+    })
+  }
+
   render () {
     const classes = ['horizMenu']
     this.props.className && classes.push(this.props.className)
+    this.state.overflows && classes.push('overflows')
 
     return (
-      <_HorizMenu className={classes.join(' ')}>
-        <div className='scrollable'>
+      <_HorizMenu ref className={classes.join(' ')}>
+        <div className='scrollable' ref={this.scrollableRef}>
           {this.props.links.map(l => <a key={l.id} onClick={this.scrollTo} href={`#_${l.id}`}>{l.label}</a>)}
         </div>
       </_HorizMenu>
