9efe5e1394062a0569d3bbda0d234e5c0a87e404
Alessio Carnevale <alessio.carnevale@gmail.com>
2018-12-07 20:39:08 +0100

    adding manifest

--- src/server.js
+++ src/server.js
@@ -52,6 +52,15 @@ function htmlTemplate (reactDom, styleTags, routeData) {
       <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=contain">
       <title>Santa Pi</title>
       <link href="assets/base.css" rel="stylesheet" type="text/css"></link>
+      <link rel="apple-touch-icon" sizes="180x180" href="/assets/apple-touch-icon.png">
+      <link rel="icon" type="image/png" sizes="32x32" href="/assets/favicon-32x32.png">
+      <link rel="icon" type="image/png" sizes="16x16" href="/assets/favicon-16x16.png">
+      <link rel="manifest" href="/assets/site.webmanifest">
+      <link rel="mask-icon" href="/assets/safari-pinned-tab.svg" color="#5bbad5">
+      <link rel="shortcut icon" href="/assets/favicon.ico">
+      <meta name="msapplication-TileColor" content="#da532c">
+      <meta name="msapplication-config" content="/assets/browserconfig.xml">
+      <meta name="theme-color" content="#000000">      
       ${styleTags}
     </head>
     
@@ -66,7 +75,7 @@ function htmlTemplate (reactDom, styleTags, routeData) {
   )}
       </script>      
       <script src="./app.bundle.js"></script>
-      
+
       <!-- Global site tag (gtag.js) - Google Analytics -->
       <script async src="https://www.googletagmanager.com/gtag/js?id=UA-130562434-1"></script>
       <script>
