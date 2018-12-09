552a8f2fb7aef3c82bc13dd91cf1dfef7f7e8453
Alessio Carnevale <alessio.carnevale@gmail.com>
2018-12-09 18:40:41 +0100

    disable HashLink prop

--- src/components/hashLink.jsx
+++ src/components/hashLink.jsx
@@ -3,14 +3,16 @@ import React, { PureComponent } from 'react'
 export default class HashLink extends PureComponent {
   scrollTo (e) {
     e.preventDefault()
-    document.querySelector(`${e.target.closest('a').hash}`).scrollIntoView({ behavior: 'smooth', block: 'start' })
+    if (!this.props.disabled) {
+      document.querySelector(`${e.target.closest('a').hash}`).scrollIntoView({ behavior: 'smooth', block: 'start' })
+    }
   }
 
   render () {
     const classes = ['HashLink']
     this.props.className && classes.push(this.props.className)
     return (
-      <a className={classes.join(' ')} onClick={this.scrollTo} href={`#${this.props.id}`}>{this.props.children}</a>
+      <a className={classes.join(' ')} onClick={this.scrollTo.bind(this)} href={`#${this.props.id}`}>{this.props.children}</a>
     )
   }
 }
